edf26b0d5b197e9d218b5f344b3d11217fb9b443
nikitaalaca <egorkin.n58@gmail.com>
2026-06-02 15:23:13 +0300

    Delete catalog.js

--- catalog.js
+++
@@ -1,8 +0,0 @@
-
-const wheelCatalog=[
-{name:'BBS CH-R',image:'https://via.placeholder.com/300?text=BBS'},
-{name:'Vossen HF-5',image:'https://via.placeholder.com/300?text=Vossen'},
-{name:'Rays TE37',image:'https://via.placeholder.com/300?text=TE37'},
-{name:'Rotiform RSE',image:'https://via.placeholder.com/300?text=Rotiform'},
-{name:'OZ Ultraleggera',image:'https://via.placeholder.com/300?text=OZ'}
-];
